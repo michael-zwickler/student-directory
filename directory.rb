c08c52cf0850dcdf1f892dcfab77ee1c4904a946
Michael Zwickler <zwickler.michael@googlemail.com>
2021-12-07 16:36:42 +0100

    method added so the user types in the student names

--- directory.rb
+++ directory.rb
@@ -1,21 +1,19 @@
-# Put students into an array
-students = [
-  {name: 'Dr. hannibal Lecter', cohort: :november},
-  {name: 'Darth Vader', cohort: :november},
-  {name: 'Nurse Ratched', cohort: :november},
-  {name: 'Michael Coreleone', cohort: :november},
-  {name: 'Alex DeLarge', cohort: :november},
-  {name: 'The Wicked Witch of the West', cohort: :november},
-  {name: 'Terminator', cohort: :november},
-  {name: 'Freddy Krueger', cohort: :november},
-  {name: 'The Joker', cohort: :november},
-  {name: 'Joffrey Baratheon', cohort: :november},
-  {name: 'Norman Bates', cohort: :november}
-]
-
 def print_header
   puts 'The students of Villains Academy'
   puts '--------------------------------'
+end
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  students = []
+  name = gets.chomp
+  while !name.empty?
+    students.push({name: name, cohort: :november}) 
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+  return students
 end
 
 def print(students)
@@ -29,6 +27,7 @@ def print_footer(students)
 end
 
 # Method calls
+students = input_students()
 print_header()
 print(students)
 print_footer(students)
