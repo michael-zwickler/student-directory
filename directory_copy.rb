abd9445174dfe485b53527126f7def590bd39204
Michael Zwickler <zwickler.michael@googlemail.com>
2021-12-08 12:01:34 +0100

    added defaults values if input is empty

--- directory_copy.rb
+++ directory_copy.rb
@@ -11,14 +11,23 @@ def input_students
   print 'Type quit if you are finished with adding students, else hit enter '
   while gets.chomp != 'quit'
     puts 'Please enter student information: '
+    
     print 'name: '
     name = gets.chomp
+    name = 'default' if name.empty?
+    
     print 'cohort: '
-    cohort = gets.chomp
+    cohort = gets.chomp.to_sym
+    cohort = 'default' if cohort.empty?
+    
     print 'height: '
     height = gets.chomp
+    height = 'default' if height.empty?
+    
     print 'country of birth: '
     cob = gets.chomp
+    cob = 'default' if cob.empty?
+
     students.push( {name: name, cohort: cohort, height: height, cob: cob} )
     puts "You have now #{students.length} student in the register"
     puts
@@ -31,10 +40,7 @@ def print_students(students)
 
   count = 0
   while count < students.length
-    name = students[count][:name]
-    cohort = students[count][:cohort]
-
-    puts "#{count + 1}: #{name} (#{cohort} cohort)"
+    puts "#{count + 1}: #{students[count]}"
     count += 1
   end
 end  
